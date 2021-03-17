a52fa0dbf1d3f533be902db47e3237e0086acc4a
TNathusius <tnathusi@mail.uni-mannheim.de>
2021-03-17 22:12:28 +0100

    Finalize Transformer

--- kge/model/embedder/transformer_lookup_embedder.py
+++ kge/model/embedder/transformer_lookup_embedder.py
@@ -19,58 +19,53 @@ class TransformerLookupEmbedder(MentionEmbedder):
         super().__init__(
             config, dataset, configuration_key, vocab_size, init_for_load_only=init_for_load_only)
         self._pooling = self.get_option("pooling")
-        self._dropout = 0.1
-        self._dimensions = self.get_option("dim")
+        if self._pooling == "cls":
+            self.cls_emb = torch.nn.parameter.Parameter(torch.zeros(self.dim))
+
+        self._dropout = self.get_option("dropout")
         self._nheads = self.get_option("nhead")
         self._dim_ff_layer = self.get_option("dim_ff")
         self._num_layers = self.get_option("num_layers")
-        self._layer_norm = nn.LayerNorm(self._dimensions)
-        encoder_layer = torch.nn.TransformerEncoderLayer(d_model=self._dimensions, nhead=self._nheads,
+        self._layer_norm = nn.LayerNorm(self.dim)
+
+        encoder_layer = torch.nn.TransformerEncoderLayer(d_model=self.dim, nhead=self._nheads,
                                                          dim_feedforward=self._dim_ff_layer, dropout=self._dropout)
         self._encoder_transformer = torch.nn.TransformerEncoder(encoder_layer, self._num_layers, self._layer_norm)
-        self.pos_encoder = PositionalEncoding(self._dimensions, 100, 0.1)
-        self._decoder = nn.Linear(self._dimensions, self._dimensions)
-        self._device = self.config.get("job.device")
 
-        for layer in self._encoder_transformer.layers:
-            self.initialize(layer.linear1.weight.data)
-            self.initialize(layer.linear2.weight.data)
-            self.initialize(layer.self_attn.out_proj.weight.data)
+        self._maxlen = self._token_lookup.shape[1]
+        if self._pooling == "cls":
+            self._maxlen += 1
+        self.pos_encoder = PositionalEncoding(self.dim, self._maxlen, self._dropout)
 
-            if layer.self_attn._qkv_same_embed_dim:
-                self.initialize(layer.self_attn.in_proj_weight)
-            else:
-                self.initialize(layer.self_attn.q_proj_weight)
-                self.initialize(layer.self_attn.k_proj_weight)
-                self.initialize(layer.self_attn.v_proj_weight)
-                
     def _token_embed(self, token_indexes):
-
-        token_embeddings = self.embed_tokens(token_indexes.long()).permute(1,0,2)
+        # prepare input for transformer encoder
+        token_embeddings = self.embed_tokens(token_indexes.long()).permute(1, 0, 2)
         src_key_padding_mask = (token_indexes == 0)
-        transformer_input = token_embeddings * math.sqrt(self._dimensions)
+        if self._pooling == 'cls':
+            # add cls token at the beginning and adjust src_key_padding_mask
+            token_embeddings = torch.cat([self.cls_emb.repeat(1, token_embeddings.shape[1], 1), token_embeddings], dim=0)
+            src_key_padding_mask = torch.cat([
+                torch.zeros([src_key_padding_mask.shape[0], 1], dtype=torch.bool), src_key_padding_mask], dim=1)
+        transformer_input = token_embeddings * math.sqrt(self.dim)
         transformer_input = self.pos_encoder(transformer_input)
-        transformer_input = (~src_key_padding_mask * transformer_input.permute(2, 1, 0)).permute(2,1,0)
-        # src_mask = torch.nn.Transformer.generate_square_subsequent_mask(self, transformer_input.size()[0]).to(
-        #    self._device)
-        encoded = self._encoder_transformer(transformer_input,
-                                            src_key_padding_mask=src_key_padding_mask)
-        encoded = (~src_key_padding_mask * encoded.permute(2, 1, 0)).permute(2,1,0)
-        decoded = self._decoder(encoded)
-        decoded = (~src_key_padding_mask * decoded.permute(2, 1, 0)).permute(1,2,0)
-
-        if self._pooling == 'max':  # should reduce dimensions to (batch_size, dim)
-            pooled_embeddings = decoded.max(dim=1).values
-        elif self._pooling == 'mean':
-            lengths = (token_indexes > 0).sum(dim=1)
-            pooled_embeddings = decoded.sum(dim=1) / lengths.unsqueeze(1)
-        elif self._pooling == 'sum':
-            pooled_embeddings = decoded.sum(dim=1)
-        elif self._pooling == 'pos':
-            pooled_embeddings = decoded[:,0,:]
+        # transformer encoder
+        encoded = self._encoder_transformer(
+            transformer_input, src_key_padding_mask=src_key_padding_mask)
+        # pool result
+        if self._pooling == 'cls':
+            return encoded[0, :, :]
         else:
-            raise NotImplementedError
-        return pooled_embeddings
+            # set embeddings of padding to 0
+            encoded = (~src_key_padding_mask * encoded.permute(2, 1, 0)).permute(1, 2, 0)
+            if self._pooling == 'max':
+                return encoded.max(dim=1).values
+            elif self._pooling == 'mean':
+                lengths = (~src_key_padding_mask).sum(dim=1)
+                return encoded.sum(dim=1) / lengths.unsqueeze(1)
+            elif self._pooling == 'sum':
+                return encoded.sum(dim=1)
+            else:
+                raise NotImplementedError
 
 
 class PositionalEncoding(nn.Module):
@@ -103,8 +98,7 @@ class PositionalEncoding(nn.Module):
         super().__init__(
             config, dataset, configuration_key, vocab_size, init_for_load_only=init_for_load_only)
 
-        self._dimensions = self.get_option("dim")
-        encoder_layer = torch.nn.TransformerEncoderLayer(d_model=self._dimensions, nhead=self.get_option("nhead"), dim_feedforward = self.get_option("dim_ff"))
+        encoder_layer = torch.nn.TransformerEncoderLayer(d_model=self.dim, nhead=self.get_option("nhead"), dim_feedforward = self.get_option("dim_ff"))
         self._encoder_transformer = torch.nn.TransformerEncoder(encoder_layer, self.get_option("num_layers"))
 
     def _token_embed(self, token_indexes):
